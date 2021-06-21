557f00a028ed564d3e67f6ad622f754dd659dfc9
Jake Peck <jakepeck44@gmail.com>
2021-06-21 15:53:49 -0500

    double checking my commit went through.  still trying to implement state in barbers.js

--- client/src/pages/Barbers.js
+++ client/src/pages/Barbers.js
@@ -39,6 +39,7 @@ const Barbers = () => {
         'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.nEOMuDcHj8YG6qOnznW7dAHaEf%26pid%3DApi&f=1'
     }
   ]
+  // setSelectedBarber(allBarbers[1])
 
   const barbersMap = allBarbers.map((barber, idx) => (
     <div key={idx}>
@@ -51,7 +52,6 @@ const Barbers = () => {
   ))
 
   console.log(allBarbers[1])
-  // setSelectedBarber(allBarbers[1])
 
   return (
     <div>
