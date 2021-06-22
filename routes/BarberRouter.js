b952afaa9122ffe4f1be6926c6a8f0cb5c4919f1
Emily Westerback <emilywesterback2@gmail.com>
2021-06-22 11:46:02 -0700

    barber seed and routes

--- routes/BarberRouter.js
+++ routes/BarberRouter.js
@@ -2,5 +2,8 @@ const router = require('express').Router()
 const controller = require('../controllers/BarberController')
 
 router.post('/create', controller.Create)
+router.delete('/:barber_id', controller.DeleteBarber)
+router.get('/all', controller.FindAllBarbers)
+router.get('/id/:barber_id', controller.FindBarber)
 
 module.exports = router
