0bf3048efdf90832f06b0534070068f64a82ab31
Jake Peck <jakepeck44@gmail.com>
2021-06-23 15:59:46 -0500

    cleaned up services.js page removed commented code and excess imports

--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -1,4 +1,3 @@
-import Nav from '../components/Nav'
 import { useEffect, useState, useReducer } from 'react'
 import axios from 'axios'
 import { BASE_URL } from '../globals'
@@ -17,33 +16,6 @@ const reducer = (state, action) => {
 }
 
 const Services = (props) => {
-  // const barberServices = [
-  //   {
-  //     serviceName: 'Haircut',
-  //     servicePrice: 10,
-  //     serviceDescription: `Not just your average haircut - you're gonna like the way you look, we guarantee it.`
-  //   },
-  //   {
-  //     serviceName: 'Beard Trim',
-  //     servicePrice: 15,
-  //     serviceDescription:
-  //       'Get cleaned up with a straight razor shave and some beard oil.'
-  //   },
-  //   {
-  //     serviceName: 'Hair Coloring',
-  //     servicePrice: 30,
-  //     serviceDescription: 'Want to look like Tekashi 6ix9ine? Now you can'
-  //   },
-  //   { serviceName: 'Fade', servicePrice: 8, serviceDescription: '' },
-  //   {
-  //     serviceName: 'Black Mask',
-  //     servicePrice: 20,
-  //     serviceDescription:
-  //       'Clean out your pores and say hello to smooth skin with one of our signature black masks'
-  //   }
-  // ]
-
-  // const [services, setServices] = useState(null)
   const [state, dispatch] = useReducer(reducer, iState)
 
   useEffect(() => {
@@ -53,7 +25,6 @@ const Services = (props) => {
   const findAllServices = async () => {
     const res = await axios.get(`${BASE_URL}/services/all`)
     dispatch({ type: 'setServices', payload: res.data })
-    // console.log(state.services)
   }
 
   const servicesMapped = state.services.map((service, idx) => (
