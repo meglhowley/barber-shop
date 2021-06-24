d398bf849aa6d6acb617a892e23a18df998178d3
Jake Peck <jakepeck44@gmail.com>
2021-06-24 11:44:14 -0500

    reviewCard displaying proper data, added buttons into editableReviewCard

--- client/src/components/EditableReviewCard.js
+++ client/src/components/EditableReviewCard.js
@@ -1,5 +1,15 @@
 const EditableReviewCard = (props) => {
-  return <div>Editable Review Card</div>
+  return (
+    <div className="reviewCard">
+      Editable Review Card
+      <h2>{`${props.review.User.firstName} ${props.review.User.lastName[0]}.`}</h2>
+      <h3>{props.review.createdAt}</h3>
+      <Rating value={props.review.star} readOnly />
+      <h3>{props.review.content}</h3>
+      <button>Edit Review</button>
+      <button>Delete Review</button>
+    </div>
+  )
 }
 
 export default EditableReviewCard
