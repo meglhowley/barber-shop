17e7b63953a03f1041149a697ffbd50a45cf8bc0
Emily Westerback <emilywesterback2@gmail.com>
2021-06-24 10:09:25 -0700

    added middleware for routes

--- routes/ReviewRouter.js
+++ routes/ReviewRouter.js
@@ -1,10 +1,30 @@
 const router = require('express').Router()
 const controller = require('../controllers/ReviewController')
 
-router.post('/create', controller.CreateReview)
-router.delete('/:review_id', controller.DeleteReview)
+router.post(
+  '/create',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CreateReview
+)
+router.delete(
+  '/:review_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.DeleteReview
+)
 router.get('/all', controller.FindAllReviews)
 router.get('/id/:review_id', controller.FindReviewById)
-router.get('/user/:user_id', controller.FindReviewByUserId)
-router.put('/:review_id', controller.UpdateReview)
+router.get(
+  '/user',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.FindReviewByUserId
+)
+router.put(
+  '/:review_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdateReview
+)
 module.exports = router
